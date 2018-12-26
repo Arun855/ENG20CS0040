2271a90d9993438f73c1f31bc8b72435f702e897
Prabhutva <42607655+Prabhutva@users.noreply.github.com>
2018-12-27 01:02:35 +0530

    Added proper login and lock functionality
    After this working on the activities and different sections is a green light.

--- app/src/main/java/com/mysterium/a1pra/helpinghand/HomeActivity.java
+++ app/src/main/java/com/mysterium/a1pra/helpinghand/HomeActivity.java
@@ -1,13 +1,90 @@
 package com.mysterium.a1pra.helpinghand;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TextView welcomeTv;
+    Button notes, reminder, expenses, lockButton;
+    String username;
+    String password;
+    String usernameCheck;
+    String passwordCheck;
+    String name;
+
+    SharedPreferences sharedPreferences;
+    PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        welcomeTv=findViewById(R.id.welcome_tv);
+        notes=findViewById(R.id.notes_b);
+        reminder=findViewById(R.id.reminder_b);
+        expenses=findViewById(R.id.expenses_b);
+        lockButton=findViewById(R.id.lock_b);
+
+        name=getIntent().getStringExtra("name");
+        username=getIntent().getStringExtra("username1");
+        password=getIntent().getStringExtra("password1");
+        usernameCheck=getIntent().getStringExtra("usernamecheck");
+        passwordCheck=getIntent().getStringExtra("passwordcheck");
+        welcomeTv.setText("Hi "+name+" !");
+
+        notes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomeActivity.this, com.mysterium.a1pra.helpinghand.mynotes.MyNotesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        reminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomeActivity.this, com.mysterium.a1pra.helpinghand.reminders.RemindersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        expenses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomeActivity.this, com.mysterium.a1pra.helpinghand.expenses.ExpensesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        lockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(HomeActivity.this,LoginActivity.class);
+                username="user";
+                password="pass";
+
+                sharedPreferences = getSharedPreferences("myPref", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.remove("username");
+                editor.remove("password");
+
+                editor.commit();
+
+                startActivity(intent1);
+                finish();
+            }
+        });
+
     }
+
+
+
 }
--- app/src/main/java/com/mysterium/a1pra/helpinghand/LoginActivity.java
+++ app/src/main/java/com/mysterium/a1pra/helpinghand/LoginActivity.java
@@ -1,13 +1,162 @@
 package com.mysterium.a1pra.helpinghand;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText usernameEt, passwordEt, nameEt;
+    Button loginButton, clearButton;
+    String username, password, name, usernameCheck, passwordCheck;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        usernameEt=findViewById(R.id.user_et);
+        passwordEt=findViewById(R.id.pass_et);
+        nameEt=findViewById(R.id.name_et);
+        loginButton=findViewById(R.id.login_b);
+        clearButton=findViewById(R.id.clear_b);
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("myPref", MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+
+
+        username = sharedPreferences.getString("username", null);
+        password = sharedPreferences.getString("password", null);
+        usernameCheck = sharedPreferences.getString("checkuser", null);
+        passwordCheck = sharedPreferences.getString("checkpassword", null);
+
+        usernameEt.setText(username);
+        passwordEt.setText(password);
+
+        if(sharedPreferences.getString("checkuser", null) == null || sharedPreferences.getString("checkpass", null) == null)
+        {
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    //change
+
+                    usernameCheck = usernameEt.getText().toString();
+                    passwordCheck = passwordEt.getText().toString();
+
+                    username=usernameCheck;
+                    password=passwordCheck;
+                    name=nameEt.getText().toString();
+
+                    Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+
+
+                        if(name.isEmpty()||username.isEmpty()||password.isEmpty())
+                        {
+                            Toast.makeText(LoginActivity.this, "Please input valid data!!", Toast.LENGTH_SHORT).show();
+                        }
+
+                        else
+                        {
+
+                            intent.putExtra("name", name);
+                            editor.putString("name", name);
+                            editor.commit();
+                            intent.putExtra("username", username);
+                            intent.putExtra("password", password);
+                            intent.putExtra("usernamecheck", usernameCheck);
+                            intent.putExtra("passwordcheck", passwordCheck);
+                            startActivity(intent);
+                            finish();
+
+                        }
+
+
+
+                    editor.putString("username", username);
+                    editor.putString("password", password);
+                    editor.putString("checkuser", usernameCheck);
+                    editor.putString("checkpass", passwordCheck);
+                    editor.commit();
+
+
+
+
+
+
+                }
+            });
+
+        }
+        else if(sharedPreferences.getString("username", null) == null && sharedPreferences.getString("password", null) == null)//when the data is locked.
+        {
+
+            nameEt.setVisibility(View.GONE);
+            loginButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    username=usernameEt.getText().toString();
+                    password=passwordEt.getText().toString();
+
+                    usernameCheck = sharedPreferences.getString("checkuser", null);
+                    passwordCheck = sharedPreferences.getString("checkpass",null);
+                    if(username.equals(usernameCheck)&&password.equals(passwordCheck))
+                    {
+                        Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+
+                        name = sharedPreferences.getString("name", null);
+                        intent.putExtra("username1", username);
+                        intent.putExtra("password1", password);
+                        intent.putExtra("name", name);
+                        intent.putExtra("usernamecheck", usernameCheck);
+                        intent.putExtra("passwordcheck", passwordCheck);
+                        startActivity(intent);
+                        finish();
+                        editor.putString("username", username);
+                        editor.putString("password", password);
+                        editor.commit();
+
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this, "Privacy Invader go attack apps of other groups :P", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+
+
+        }
+        else
+        {
+            Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+            username = sharedPreferences.getString("username", null);
+            password = sharedPreferences.getString("password", null);
+            usernameCheck = sharedPreferences.getString("checkuser", null);
+            passwordCheck = sharedPreferences.getString("checkpass",null);
+            name = sharedPreferences.getString("name", null);
+
+            usernameEt.setText(username);
+            passwordEt.setText(password);
+
+            intent.putExtra("username1", username);
+            intent.putExtra("password1", password);
+            intent.putExtra("usernamecheck", usernameCheck);
+            intent.putExtra("passwordcheck", passwordCheck);
+            intent.putExtra("name", name);
+            startActivity(intent);
+            finish();
+        }
+        //else go to next activity
+
+
+
     }
 }
